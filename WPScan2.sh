972c9844a562ee12f1bdc8da5ef3f7076806f6d5
2096779623 <57583560+2096779623@users.noreply.github.com>
2021-12-10 16:35:17 +0800

    Update WPScan2.sh

--- WPScan2.sh
+++ WPScan2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#/data/data/com.termux/files/usr/bin/bash
 biru="\033[0;34m"
 kuning="\033[1;33m"
 merah="\033[1;31m"
@@ -7,40 +7,35 @@ putih="\033[1;37m"
 ungu="\033[0;35m"
 clear
 sleep 2
-echo "$merah Tools By ./Firman_ID"
+echo "$merah Tools By 2096779623"
 sleep 0.4
 echo "$hijau [*]===============================================[*]$ungu"
-echo "  ||______________TOOLS INSTALL WPScan______________||$kuning"
-echo "  || 1.Install WPScan$merah"
+echo "  ||______________WPScan安装工具______________||$kuning"
+echo "  || 1.安装WPScan$merah"
 echo "  || 2.Exit$hijau"
-echo "|-Masukan Piliha Anda Di Sini->$kuning"
-read -p "|-[Firman_ID]--> " lagu
-if [ "$lagu" = "1" ]
-then
+echo "|-在此处输入您的选择：->$kuning"
 clear
 sleep 1
-echo "$kuning >>>>>>>>>Memulai Penginstallan WPScan<<<<<<<<< $merah"
+echo "$kuning >>>>>>>>>开始安装WPScan<<<<<<<<< $merah"
 cd $HOME
-pkg update && pkg upgrade -y
-pkg install git
-pkg install ruby
-git clone https://github.com/wpscanteam/wpscan
-pkg install ruby
-gem install bundl
+pkg up && pkg upgrade -y
+pkg i git ruby -y
+git clone https://hub.fastgit.org/wpscanteam/wpscan
+gem install bundle
 sleep 2
-echo "$merah Loading......."
+echo "$merah 加载中......."
 sleep 4
-echo "$putih WPScan Terpasang!!..."
+echo "$putih WPScan 已安装!..."
 sleep 2
-echo "$hijau Terima Kasih Ka,Telah Berkunjung...."
+echo "$hijau 谢谢你的拜访...."
 elif [ "$lagu" = "2" ]
 then
 sleep 2
-echo "oke thank kak,terima kasih telah mencoba"
-echo "tools saya,semoga hari kaka menyenangkan......"
+echo "好的，谢谢，谢谢你的尝试"
+echo "我的工具，祝你有个愉快的一天兄弟......"
 sleep 3
 else
-echo "$merah Maaf Input Yang Kaka Masukan Salah"
+echo "$merah 抱歉，您输入的内容有误"
 sleep 3
-sh WPScan2.sh
+bash WPScan2.sh
 fi
